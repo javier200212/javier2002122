77402ad86a2c9f1a306b6701b75c901c9b6aea2c
javier200212 <javier2580764@gmail.com>
2024-11-06 23:29:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Función para mostrar la sorpresa
+function showSurprise() {
+    const sorpresa = document.getElementById('sorpresa');
+    sorpresa.style.display = 'block';
+    sorpresa.classList.add('hidden'); // Aplica la animación de fadeIn
+}
